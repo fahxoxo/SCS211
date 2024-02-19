2c358b715292e8eb120c61af55e7d8898701285e
Fahxoxo <fahpemika@gmail.com>
2024-02-19 09:29:40 +0700

    1

---
+++ week12/tc.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>มูลนิธิศูนย์เกิดใหม่ (หญิง) REBIRTH CENTER FOUNDETION</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">  
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300;400&display=swap" rel="stylesheet">
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    </head>
+
+<body>
+
+<?php include("nav.php"); ?>
+
+
+
+  <!-- welcome section -->
+  <section class="welcome_section layout_padding">
+    <div class="container">
+      <div class="custom_heading-container center">
+        <h2>
+          T.C. คืออะไร ?
+        </h2>
+      </div>
+      <div class="layout_padding2">
+        <div class="img-box">
+          <img src="img/tc.jpg" alt=""/>
+        </div>
+        <div class="detail-box">
+          <p class="fs-4 fw-normal yor">
+ชุมชนบำบัด - T.C. หรือ Therapeutic Community เป็นกระบวนการในการบำบัดรักษาที่ให้ความช่วยเหลือ เพื่อให้ผู้เข้ารับการอบรมสามารถช่วยเหลือตนเองได้ โดยจำลองรูปแบบการดำรงชีวิตในสังคม เพื่อสร้างการพัฒนาที่ครอบคลุมในด้านต่างๆ ของบุคคลที่เข้ารับการอบรมและพัฒนาจิตใจ โดยเริ่มตั้งแต่การจัดการพฤติกรรม ความสามารถ แนวความคิด วุฒิภาวะและประสบการณ์ของแต่ละบุคคล ให้เกิดการเรียนรู้ เกิดความเข้าใจในตัวเอง และเสริมสร้างความมั่นใจ จนสามารถกลับไปใช้ชีวิตปกติได้แบบสุขเป็น ทุกข์เป็น โดยที่ไม่ต้องพึงพายาเสพติดอีก
+</p><br /><br /><br />
+<p class="fs-4 fw-normal yor">
+ชุมชนบำบัดนั้น มีความเชื่อที่ว่าบุคคล สามารถเปลี่ยนแปลงได้ โดยอาศัยกลุ่มสังคมในชุมชนบำบัดที่จะเป็นแรงผลักดัน เพื่อทำให้เกิดการเปลี่ยนแปลง อันประกอบไปด้วย การให้บทบาทและความรับผิดชอบ ในโครงสร้างที่เหมาะสม เพื่อเป็นการฝึกฝนจนสร้างเป็นอุปนิสัย
+</p><br /><br /><br />
+<p class="fs-4 fw-normal yor">จากแนวคิด ของ Maxwell Jones (มงคล มณฑา, 2544) หลักการทำงานและการดำเนินการกลุ่มของชุมชนบำบัดสามารถแบ่งได้ 6 ประเภท ดังนี้
+</p><br />
+    <ul>
+      <li>สามารถมีการสื่อสารถึงกันได้ทุกระดับ</li>
+      <li>ทุกระดับสามารถทำการตัดสินใจได้</li>
+      <li>การผลัดเปลี่ยนบทบาทการเป็นหัวหน้า</li>
+      <li>ถือมติส่วนใหญ่ในการตัดสินใจ</li>
+      <li>หลักการเรียนรู้ทางสังคมนั้น เกิดจากการมีปฏิกริยาการตอบโต้ซึ่งกันและกันในทางสังคม โดนยึดถือหลักที่นี้และเดี๋ยวนี้ (Here & Now)</li>
+      <li>สิ่งแวดล้อมถือว่าเป็นองค์ประกอบสำคัญในการบำบัด ดังนั้น ชุมชนบำบัดจะไม่มีการรักษาเฉพาะตัว แต่จะต้องทำการวางแผนการบำบัดแบบเฉพาะตัว เพื่อช่วยให้ผู้เข้ารับการอบรมสามารถพัฒนาพฤติกรรมในกลุ่มได้</li>
+    </ul>
+<p class="fs-4 fw-normal yor">นอกจากนั้น ชุมชนบำบัด ยังยึดถือหลักการการเรียนรู้ทางสังคมแบบพึงพาตนเอง เป็นรูปแบบในกระบวนการเรียนรู้ (Self-help Social Learning Treatment Program Model) โดยมีความเชื่อที่ว่า
+</p><br />
+    <ul>
+      <li>การเรียนรู้เกิดจาก สิ่งเร้า การท้าทาย และการลงมือทำ ปฏิบัติจริง</li>
+      <li>ความเข้าใจในมนุษย์ และการเป็นสากลของมนุษย์ (คนเหมือนคน แต่คนไม่เหมือนกัน)</li>
+      <li>การเปิดเผยตัวตน ที่ไม่ใช่เพียงแค่การแสดงออกทางพฤติกรรม แต่รวมไปถึง ความรู้สึกนึกคิด ความหวาดกลัว ความหวัง ฯลฯ</li>
+      <li>การเข้าถึงตัวตนภายใน ทั้งอย่างที่เป็นทางการและไม่เป็นทางการ โดยคณะเจ้าหน้าที่</li>
+    </ul>
+<p class="fs-4 fw-normal yor">โดยมีวัตถูประสงค์เบื้องต้นคือ
+</p><br />
+    <ul>
+      <li>สามารถดำรงชีวิตโดยไม่พึงพายาเสพติดได้</li>
+      <li>สามารถเปลี่ยนวิถีการดำเนินชีวิตได้</li>
+      <li>สามารถสร้างทัศนคติที่ดีต่อตนเองได้ รู้สึกถึงความมีคุณค่าในตนเอง เพื่อให้เกิดความเชื่อมั่นตนเอง และลดพฤติกรรมการต่อต้านจากสังคม</li>
+      <li>สามารถทำงานร่วมกับผู้อื่นได้</li>
+    </ul>
+<p class="fs-4 fw-normal yor">ชุมชนบำบัดนั้น อาศัยทฤษฎีพื้นฐานการเรียนรู้ทางสังคม (Social Learning Theory) ประกอบไปด้วย
+</p><br />
+    <ul>
+      <li>กระบวนการของกลุ่ม</li>
+      <li>พลังจากกลุ่มเพื่อน (Positive Peer Presure)</li>
+      <li>การสังเกตและบันทึกพฤติกรรม</li>
+      <li>การเป็นแบบอย่างที่ดี (Role Model)</li>
+      <li>การเรียนรู้จากประสบการณ์ (Learning Experience)</li>
+      <li>ความเชื่อในระบบภายในชุมชน (System Belief)</li>
+      <li>อุปสรรคมีผลต่อการพัฒนาพฤติกรรม</li>
+    </ul>
+<br /><br /><br />
+ <footer class="blockquote-footer">อ้างอิงจากหนังสือ <cite title="Source Title">รู้ลึก...ชุมชนบำบัด โดย นรัญญา ศรีบูรพา</cite></footer>
+</blockquote>
+
+
+ 
+          </p>
+
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <!-- end welcome section -->
+    
+
+   
+
+
+  
+
+
+  
+  <?php include("footer.php"); ?>
+
+
+  </body>
+
+</html>
