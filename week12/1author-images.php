8f9f92df0d5ff5eea03895415d79b36032918744
Fahxoxo <fahpemika@gmail.com>
2024-02-19 10:16:21 +0700

    data for gallery

---
+++ week12/1author-images.php
@@ -0,0 +1,9 @@
+<?php
+$author_images = [
+    "_pic1" => "https://rebirth-center.org/img/testimonial-1.jpg",
+    "_pic2" => "https://rebirth-center.org/img/testimonial-2.jpg",
+    "_pic3" => "https://rebirth-center.org/img/testimonial-3.jpg",
+];
+
+
+?>
---
+++ week12/1vertical-list.php
@@ -0,0 +1,60 @@
+
+<div class="row g-5">
+                <div class="col-xl-4 col-lg-6">
+                    <div class="bg-light rounded overflow-hidden">
+                        <img class="img-fluid w-100" src="img.png" alt="">
+                        <div class="p-4">
+                            <a class="h3 d-block mb-3" href="">TC คืออะไร</a>
+                            <p class="m-0">เข้ามาเรียนรู้ว่าโปรแกรม TC คืออะไร ทำไมจึงรักษาผู้ติดยาได้ผล</p>
+                        </div>
+                        <div class="d-flex justify-content-between border-top p-4">
+                            <div class="d-flex align-items-center">
+                                <img class="rounded-circle me-2" src="img/png/apple-touch-icon.png" width="25" height="25" alt="">
+                                <small>Rebirth Center</small>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <small class="ms-3"><i class="far fa-eye text-primary me-1"></i>508</small>
+                                <small class="ms-3"><i class="far fa-comment text-primary me-1"></i>0</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6">
+                    <div class="bg-light rounded overflow-hidden">
+                        <img class="img-fluid w-100" src="img/0.jpg" alt="">
+                        <div class="p-4">
+                            <a class="h3 d-block mb-3" href="">คำถามที่พบบ่อย</a>
+                            <p class="m-0">ตอบทุกข้อสงสัยในใจคุณ</p>
+                        </div>
+                        <div class="d-flex justify-content-between border-top p-4">
+                            <div class="d-flex align-items-center">
+                                <img class="rounded-circle me-2" src="img/png/apple-touch-icon.png" width="25" height="25" alt="">
+                                <small>Rebirth Center</small>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <small class="ms-3"><i class="far fa-eye text-primary me-1"></i>1146</small>
+                                <small class="ms-3"><i class="far fa-comment text-primary me-1"></i>0</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6">
+                    <div class="bg-light rounded overflow-hidden">
+                        <img class="img-fluid w-100" src="img/0.jpg" alt="">
+                        <div class="p-4">
+                            <a class="h3 d-block mb-3" href="">ประวัติความเป็นมาศูนย์เกิดใหม่</a>
+                            <p class="m-0">สาเหตุที่มีการจัดตั้งศูนย์บำบัดยาเสพติด หญิง ขึ้น</p>
+                        </div>
+                        <div class="d-flex justify-content-between border-top p-4">
+                            <div class="d-flex align-items-center">
+                                <img class="rounded-circle me-2" src="img/png/apple-touch-icon.png" width="25" height="25" alt="">
+                                <small>Rebirth Center</small>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <small class="ms-3"><i class="far fa-eye text-primary me-1"></i>834</small>
+                                <small class="ms-3"><i class="far fa-comment text-primary me-1"></i>0</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
